44e6c3283944e948f5a086a115944c8639fbc960
fguini <fguini@gmail.com>
2015-10-29 17:17:31 -0300

    init match and found

--- js/controllers/matchController.js
+++ js/controllers/matchController.js
@@ -6,11 +6,30 @@ app.controller('matchController', ['$scope', '$http', function($scope, $http) {
 		{
 			user_id: 1
 		}).success(function(data){
-        	console.log(data);
     		$scope.matches = data;
     	}).error(function (err){
     		console.log(err)
 	});
+
+    $scope.matchdogs = function(_id) {
+    	$http.post('http://localhost:5000/match', 
+		{
+			id: _id
+		}).success(function(data){
+    		console.log(data);
+	    	for(var i = 0 ; i < $scope.matches.length ; i++)
+	    		if ($scope.matches[i].id == _id)
+	    			$scope.matches.splice(i, 1);
+    	}).error(function (err){
+    		console.log(err)
+		});
+    }
+
+    $scope.pass = function(id) {
+    	for(var i = 0 ; i < $scope.matches.length ; i++)
+    		if ($scope.matches[i].id == id)
+    			$scope.matches.splice(i, 1);
+    }
 
 }]);
 
